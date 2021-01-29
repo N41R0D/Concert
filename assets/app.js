176aefcb3e03a0bce2c4677230bc3490005b8302
OiseauPython <jeremy.stanko@posteo.net>
2021-01-29 09:46:53 +0100

    bob le bricoleur

--- assets/app.js
+++ assets/app.js
@@ -1,13 +1,7 @@
 import './styles/app.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './page/page-accueil/accueil';
 
-import Footer from './component/footer-component/Footer';
-
-ReactDOM.render(<>
-    <App />
-    <Footer />
-    </>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
