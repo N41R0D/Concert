1b75e16e35d3578de979445f97f0720aee6dbe33
Cloé G <cloegosalbes@gmail.com>
2021-02-04 11:33:08 +0100

    Modif mineures

--- assets/app.js
+++ assets/app.js
@@ -2,6 +2,6 @@ import './styles/app.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './page/page-accueil/accueil';
+import App from './page/page-reservation/reservation';
 
 ReactDOM.render(<App />, document.getElementById("root"));
