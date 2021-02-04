ae23bbd0c5efaef622fdb46a3385f3799931cec1
OiseauPython <jeremy.stanko@posteo.net>
2021-02-04 11:28:34 +0100

    fix

--- assets/component/card-component/card.js
+++ assets/component/card-component/card.js
@@ -11,8 +11,6 @@ class CardComponent extends React.Component {
 
   card() {
     //Object.keys(this.props)[0] retourne le premier élément du props 
-    const test = this.props.concert.date;
-    
     switch (Object.keys(this.props)[0]) {
 		case "concert":
 			return (
@@ -23,15 +21,14 @@ class CardComponent extends React.Component {
 
 					<Card.Body>
 						<Card.Title>{this.props.concert.artistName}</Card.Title>
-						<Card.Text>
-							{
-								new Intl.DateTimeFormat("fr-FR", {
-                  year: "numeric",
-                  month: "long",
-                  day: "2-digit",
-                }).format(this.props.concert.date)
-							}
-						</Card.Text>
+						<Card.Text>{this.props.concert.date}</Card.Text>
+						{/*<Card.Text>
+							{new Intl.DateTimeFormat("fr-FR", {
+								year: "numeric",
+								month: "long",
+								day: "2-digit",
+							}).format(this.props.concert.date)}
+						</Card.Text>*/}
 						<Button variant="primary">Go somewhere</Button>
 					</Card.Body>
 				</Card>
